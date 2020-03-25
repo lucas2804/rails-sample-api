99316a279fa72558985c0a4bb74e5bee6cf257d6
Luc Dang <quanglucdang2804@gmail.com>
2020-03-25 21:49:42 +0700

    migrate users

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_074827) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email"
+    t.string "name"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
