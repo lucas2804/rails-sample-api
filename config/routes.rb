c9baa0c48a60aa8e40cb49e580fb7155ed572aa8
Luc Dang <quanglucdang2804@gmail.com>
2020-05-04 21:46:37 +0700

    routes to create anonymous chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :conversations, only: [:create]do
+    resources :messages, only: [:create] # enum status: { unseen: 0, seen: 1 }
+    member do
+      delete :close
+    end
+  end
+
   resources :retweets
   root :to => "users#index"
   resources :tweets
