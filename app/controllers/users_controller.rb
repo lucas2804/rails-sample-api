825adff210c79fbb0399ccf755acccbeaa39f91a
Luc Dang <quanglucdang2804@gmail.com>
2020-08-23 16:41:16 +0700

    rm redundant files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,45 +11,6 @@ class UsersController < ApplicationController
 
   end
 
-  # GET /users
-  def index
-    page = params[:page].to_i.positive? ? params[:page].to_i : 1
-    per_page = params[:per_page].to_i.positive? ? params[:per_page].to_i : 1
-    @users = User.paginate(page: page, per_page: per_page)
-
-    render json: { users: @users, meta: meta_attributes(@tenants) }
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
