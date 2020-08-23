e9fd68c71cf24f7cb12c9c23f47ecf58d721dbaa
Luc Dang <quanglucdang2804@gmail.com>
2020-08-23 23:21:18 +0700

    update docker build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t lucdang/rails-sample-api -f ./Dockerfile
+docker build -t lucdang/rails-sample-api -f ./Dockerfile ./
 docker push lucdang/rails-sample-api:latest
 
 kubectl apply -f secret
