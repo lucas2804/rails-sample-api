31016b6bcf77fb9c797487da89a27c0b19e49732
Luc Dang <quanglucdang2804@gmail.com>
2020-08-23 22:39:48 +0700

    add configmap and secret which encode

--- app/services/users/collect_name_service.rb
+++ app/services/users/collect_name_service.rb
@@ -1,27 +1,39 @@
 module Users
   class CollectNameService
     def execute
-      feed_http_request(:get, url, nil)
-    end
-
-    private
-
-    def feed_http_request(method, url, payload = {})
-      rest_params = rest_params(method, url, payload)
+      rest_params = rest_params(:get, url, {})
       ap rest_params
       begin
         response = ::RestClient::Request.execute(rest_params)
         body = JSON.parse(response.body)
-        names = []
-        body['items'].each do |item|
-          names << item['url']&.split('/')&.last&.titleize
-        end
-        ::OpenStruct.new(result: names, error: nil)
+
+        sensitive_name_str = sensitive_names(body)
+        result = transform_content_html(sensitive_name_str, body)
+
+        ::OpenStruct.new(result: result, error: nil)
       rescue ::RestClient::ExceptionWithResponse, ::RestClient::NotFound, ::StandardError => error
         ap error
         ap error.backtrace
         ::OpenStruct.new(result: nil, error: error)
       end
+    end
+
+    private
+
+    def transform_content_html(sensitive_name_str, body)
+      regex = Regexp.new(sensitive_name_str, true)
+      body['items'].each_with_index do |item, index|
+        body['items'][index]['content_html'].gsub!(regex, '***')
+      end
+      body
+    end
+
+    def sensitive_names(body)
+      names = []
+      body['items'].each do |item|
+        names << item['url']&.split('/')&.last.titleize.split(' ')
+      end
+      names.flatten.join('|')
     end
 
     def rest_params(method, url, payload)
