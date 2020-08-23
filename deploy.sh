b0a8fc5f75ea8c37fc203b27ef8d4e5ee70835c0
Luc Dang <quanglucdang2804@gmail.com>
2020-08-23 13:40:31 +0700

    run travis ci

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-docker build -t lucdang/rails-sample-api .
+docker build -t lucdang/rails-sample-api -f ./Dockerfile
 docker push lucdang/rails-sample-api:latest
 
+kubectl apply -f secret
+kubectl apply -f auto_deploy_kubernetes
