03bafd00d1da84b6414c71dd8db1b8228cc44dac
Luc Dang <quanglucdang2804@gmail.com>
2020-08-23 23:39:56 +0700

    update sha

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-docker build -t lucdang/rails-sample-api -f ./Dockerfile ./
-docker push lucdang/rails-sample-api:latest
+docker build -t lucdang/rails-sample-api:$SHA -f ./Dockerfile ./
+docker push lucdang/rails-sample-api:$SHA
 
 kubectl apply -f secret
 kubectl apply -f auto_deploy_kubernetes
+kubectl set image deployments/rails-sample-api rails-sample-api=lucdang/rails-sample-api:$SHA
