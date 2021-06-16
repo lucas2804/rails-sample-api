e4c572a7e603aeb76c9cd6701494ec5b48cff8f2
Luc Dang <quanglucdang2804@gmail.com>
2021-06-16 21:41:37 +0700

    update health cheeck

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
   resources :follows
-  root :to => "users#collect_name"
+  root :to => "users#healthz"
   get '/healthz' => "users#healthz"
   resources :users do
     collection do
